aae015e1ba1624559700077e230e85f0b3562daa
pawel <ptulaja@gmail.com>
2018-02-16 22:06:42 +0100

    Kodilla

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,11 +1,30 @@
 package com.crud.tasks;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.slf4j.Logger;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 public class TasksApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(TasksApplication.class, args);
+
+	private static final Logger log = LoggerFactory.getLogger(TasksApplication.class);
+
+	public static void main(String[] args) throws UnknownHostException {
+
+		SpringApplication app = new SpringApplication(TasksApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		log.info("\n----------------------------------------------------------\n\t" +
+						"Application '{}' is running! Access URLs:\n\t" +
+						"Local: \t\thttp://localhost:{}\n\t" +
+						"External: \thttp://{}:{}\n----------------------------------------------------------",
+				env.getProperty("spring.application.name"),
+				env.getProperty("server.port"),
+				InetAddress.getLocalHost().getHostAddress(),
+				env.getProperty("server.port"));
 	}
 }
--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -1,12 +1,27 @@
 package com.crud.tasks.domain;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Getter
+import javax.persistence.*;
+
+
 @AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity(name = "tasks")
+@Table
 public class Task {
+    @Id
+    @GeneratedValue
+    @Column(name="id", unique = true, nullable = false)
     private Long id;
+
+    @Column(name="name")
     private String title;
+
+    @Column(name="description")
     private String content;
 }

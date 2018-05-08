66553aff72ff450c6646c45a5d91a9e0f44be053
pawel <ptulaja@gmail.com>
2018-05-08 21:01:54 +0200

    JAR and PostgreSQL fixed

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -35,5 +35,5 @@ public class TasksApplication /*extends SpringBootServletInitializer*/ {
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(TasksApplication.class);
 	}
-	*/
+*/
 }

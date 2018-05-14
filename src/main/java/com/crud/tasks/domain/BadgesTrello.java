6967f760bd39f7161edecf1471809f18d9517098
pawel <ptulaja@gmail.com>
2018-05-14 20:16:23 +0200

    kodilla

--- src/main/java/com/crud/tasks/domain/BadgesTrello.java
+++ src/main/java/com/crud/tasks/domain/BadgesTrello.java
@@ -3,7 +3,6 @@ package com.crud.tasks.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.sun.xml.internal.ws.api.message.Attachment;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
